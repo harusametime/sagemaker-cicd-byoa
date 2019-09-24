9c9b7da24aa29f406650ae0affffba987d75df09
Masaki Samejima <samejim@amazon.co.jp>
2019-09-24 18:59:10 +0900

    Update train_local.py

--- train_local.py
+++ train_local.py
@@ -25,3 +25,6 @@ if __name__ == '__main__':
                                  train_instance_count=1,
                                  train_instance_type='local')
     mnist_estimator.fit(inputs)
+
+    
+    Error_code here
