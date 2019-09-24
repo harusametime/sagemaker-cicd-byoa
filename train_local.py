e9e976df96b7429bf00faab74bc6b919b1213205
samejim <samejim@amazon.co.jp>
2019-09-24 23:08:13 +0900

    add tags

--- train_local.py
+++ train_local.py
@@ -7,12 +7,15 @@ if __name__ == '__main__':
     parser.add_argument('--entry_point', type=str)
     parser.add_argument('--src_dir', type=str)
     parser.add_argument('--input_s3', type=str)
+    parser.add_argument('--version', type=str)
+
 
     args = parser.parse_args()
 
     entry_point = args.entry_point
     src_dir = args.src_dir
     inputs = args.input_s3
+    version = args.version
 
     from sagemaker.tensorflow import TensorFlow
 
@@ -23,6 +26,7 @@ if __name__ == '__main__':
                                  training_steps=100,
                                  evaluation_steps=10,
                                  train_instance_count=1,
-                                 train_instance_type='local')
+                                 tags={"version": version},
+                                 train_instance_type='ml.m4.xlarge')
 
     mnist_estimator.fit(inputs)
